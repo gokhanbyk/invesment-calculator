4c067578ea0c5da6545754e69b1159ac1ca0632c
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-02-21 00:41:22 +0300

    Results component starter

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,8 @@
+import { calculateInvestmentResults } from '../util/investment.js'
+
 export default function Results({ input }) {
+  const resultsData = calculateInvestmentResults(input)
+
   return (
     <>
       <p>Results...</p>
